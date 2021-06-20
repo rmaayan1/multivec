12b923033f0b9fbc225afd227ad1b3e104b59c4b
Maayan Rechtman <maayan.rechtman@gmail.com>
2021-06-20 20:24:46 +0300

    Initial Commit

---
+++ src/main/java/il/org/multivec/MultivecApplication.java
@@ -0,0 +1,13 @@
+package il.org.multivec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultivecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultivecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/il/org/multivec/MultivecApplicationTests.java
@@ -0,0 +1,13 @@
+package il.org.multivec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultivecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
